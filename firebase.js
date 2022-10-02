faef11e73f0dfcebc1ad602429106e83d6e3ab0b
Ahmet Hakan Donmez <ahmetislq@gmail.com>
2022-10-02 17:26:16 +0200

    Install Firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBG_crgGvSBhN6bzn0ey4KHLC-9V9-StTo",
+
+  authDomain: "signal-clone-fc866.firebaseapp.com",
+
+  projectId: "signal-clone-fc866",
+
+  storageBucket: "signal-clone-fc866.appspot.com",
+
+  messagingSenderId: "474072168337",
+
+  appId: "1:474072168337:web:75b3f399ac6230aee1cc30",
+
+  measurementId: "G-MHMVRFB3R3",
+};
